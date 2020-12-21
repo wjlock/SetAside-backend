8bd631b739a0903b4d1890dfd487cbd58967966d
zfinnan <63361320+zfinnan@users.noreply.github.com>
2020-12-21 10:31:03 -0500

    [expenses] create schema

---
+++ models/Expenses.js
@@ -0,0 +1,97 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Expenses Schema
+const expensesSchema = new Schema ({
+    Home: {
+        Rent: {
+            type: Number
+        },
+        Utilities: {
+            Water: {
+                type: Number
+            },
+            Gas: {
+                type: Number
+            },
+            Electric: {
+                type: Number
+            }
+        },
+        Phone: {
+            type: Number
+        },
+        Internet: {
+            type: Number
+        },
+        Insurance: {
+            type: Number
+        },
+        Repairs: {
+            type: Number
+        },
+        Landscaping: {
+            type: Number
+        }
+    },
+    Daily: {
+        Groceries: {
+            type: Number
+        },
+        Childcare: {
+            type: Number
+        },
+        Laundry: {
+            type: Number
+        },
+        Restaurants: {
+            type: Number
+        },
+        Housecleaning: {
+            type: Number
+        },
+        Petcare: {
+            type: Number
+        }
+    },
+    Transportation: {
+        Gas: {
+            type: Number
+        },
+        CarInsurance: {
+            type: Number
+        },
+        Repairs: {
+            type: Number
+        },
+        Cleaning: {
+            type: Number
+        },
+        Parking: {
+            type: Number
+        },
+        PublicTransport: {
+            type: Number
+        },
+        TaxiOrUber: {
+            type: Number
+        }
+    },
+    Entertainment: {
+        Television: {
+            type: Number
+        },
+        Movies: {
+            type: Number
+        },
+        Concert: {
+            type: Number
+        },
+        Miscellaneous: {
+            type: Number
+        }
+    }
+})
+
+
+module.exports = User = mongoose.model('Expenses', expensesSchema);

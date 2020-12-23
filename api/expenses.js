acd0909d10fa143f9243511820f12bab0a7574d1
Your Name <you@example.com>
2020-12-23 09:38:38 -0500

    added changes again for today expense post route

--- api/expenses.js
+++ api/expenses.js
@@ -44,32 +44,30 @@ router.post("/new", (req, res) => {
   models.User.findOne({ _id: req.body.id })
     .then((user) => {
       models.Expense.findOne({
-        "category.name" : req.body.name,
-      }).then((foundExpenseName) => {
-        let expenseName = foundExpenseName.category.name
+        name : req.body.name,
+        category: req.body.category
+      }).then((foundExpense) => {
+        console.log(foundExpense)
 
-        if (expenseName === req.body.name) {
-          // Can narrowed down further to narrow by category name and amount ect
-          // Could say if category and name are equal to req.body category and name then that
-          // expense already exist
+      
+        if (foundExpense.category === req.body.category && foundExpense.name === req.body.name) {
           res.send({ msg: "expense already exist for this user" });
         } else {
           const newExpense = new models.Expense({
             category: req.body.category,
-            "category.name": req.body.name,
-            "category.amount": req.body.amount,
-            "category.day": req.body.day,
-            "category.month": req.body.month,
-            "category.year": req.body.year
+            name: req.body.name,
+            amount: req.body.amount,
+            day: req.body.day,
+            month: req.body.month,
+            year: req.body.year
           });
           newExpense.save();
           user.expenses.push(newExpense);
           user.save();
-          // res.status(201).json({ newExpense })
           res.send({ newExpense });
         }
-      });
     })
+  })
     .catch((error) => res.send({ error }));
 });
 
